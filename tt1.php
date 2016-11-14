a78f6f4248aa1b1921105478a24438e745a05d1c
ravuthz <ravuthz@gmail.com>
2016-11-14 15:01:25 +0700

    Prepare some code

--- tt1.php
+++ tt1.php
@@ -1,8 +1,8 @@
 <?php
 $host = "localhost";
 $user = "root";
-$pass = "123456";
-$db = "graduateproject";
+$pass = "";
+$db = "project_base_ecommoce";
 
 $link = mysqli_connect($host,$user,$pass,$db)or die(mysqli_connect_errorno());
 mysqli_query($link,"set names utf8");
